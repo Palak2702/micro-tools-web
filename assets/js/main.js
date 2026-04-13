74da39f916e53c717db6c1411eea4b6bddcf7fab
Palak Khandelwal <QD2580@qds.com>
2026-04-13 14:07:26 +0530

    added aboutUs and ContactUs pages

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,34 @@
-function loadComponent(id, file) {
-    fetch(file)
-        .then(res => res.text())
-        .then(data => {
-            document.getElementById(id).innerHTML = data;
-        })
-        .catch(err => console.error("Error loading component:", err));
+function loadComponent(id, file, callback) {
+  fetch(file)
+    .then((res) => res.text())
+    .then((data) => {
+      document.getElementById(id).innerHTML = data;
+
+      // Run callback AFTER loading
+      if (callback) callback();
+    })
+    .catch((err) => console.error("Error loading component:", err));
 }
 
-// FIX PATHS
-loadComponent("header", "/components/header.html");
+// DROPDOWN FUNCTION
+function initDropdown() {
+  const btn = document.querySelector(".dropbtn");
+  const dropdown = document.getElementById("toolsDropdown");
+
+  if (!btn || !dropdown) return; // prevents error
+
+  btn.addEventListener("click", function (e) {
+    e.stopPropagation();
+    dropdown.classList.toggle("show");
+  });
+
+  window.addEventListener("click", function (e) {
+    if (!btn.contains(e.target)) {
+      dropdown.classList.remove("show");
+    }
+  });
+}
+
+// LOAD COMPONENTS
+loadComponent("header", "/components/header.html", initDropdown);
 loadComponent("footer", "/components/footer.html");
